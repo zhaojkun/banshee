5d7c43149881e19af35e481a5cc0ef2c809b1107
hit9 <hit9@icloud.com>
2016-03-17 12:13:06 +0800

    Add an warning to graphite format pattern

--- static/src/services/Util.js
+++ static/src/services/Util.js
@@ -1,33 +1,62 @@
 /*@ngInject*/
 module.exports = function () {
-  return {
-    buildRepr: function (rule) {
-      var parts = [];
+  var exports = {};
+  exports.buildRepr = function (rule) {
+    var parts = [];
 
-      var trendUp = rule.trendUp || false;
-      var trendDown = rule.trendDown || false;
-      var thresholdMax = rule.thresholdMax || 0;
-      var thresholdMin = rule.thresholdMin || 0;
+    var trendUp = rule.trendUp || false;
+    var trendDown = rule.trendDown || false;
+    var thresholdMax = rule.thresholdMax || 0;
+    var thresholdMin = rule.thresholdMin || 0;
 
-      if (trendUp && thresholdMax === 0) {
-        parts.push('trend ↑');
-      }
-      if (trendUp && thresholdMax !== 0) {
-        parts.push('(trend ↑ && value >= ' + parseFloat(thresholdMax.toFixed(3)) + ')');
-      }
-      if (!trendUp && thresholdMax !== 0) {
-        parts.push('value >= ' + parseFloat(thresholdMax.toFixed(3)));
-      }
-      if (trendDown && thresholdMin === 0) {
-        parts.push('trend ↓');
-      }
-      if (trendDown && thresholdMin !== 0) {
-        parts.push('(trend ↓ && value <= ' + parseFloat(thresholdMin.toFixed(3)) + ')');
-      }
-      if (!trendDown && thresholdMin !== 0) {
-        parts.push('value <= ' + parseFloat(thresholdMin.toFixed(3)));
-      }
-      return parts.join(' || ');
+    if (trendUp && thresholdMax === 0) {
+      parts.push('trend ↑');
+    }
+    if (trendUp && thresholdMax !== 0) {
+      parts.push('(trend ↑ && value >= ' + parseFloat(thresholdMax.toFixed(3)) + ')');
+    }
+    if (!trendUp && thresholdMax !== 0) {
+      parts.push('value >= ' + parseFloat(thresholdMax.toFixed(3)));
+    }
+    if (trendDown && thresholdMin === 0) {
+      parts.push('trend ↓');
+    }
+    if (trendDown && thresholdMin !== 0) {
+      parts.push('(trend ↓ && value <= ' + parseFloat(thresholdMin.toFixed(3)) + ')');
+    }
+    if (!trendDown && thresholdMin !== 0) {
+      parts.push('value <= ' + parseFloat(thresholdMin.toFixed(3)));
+    }
+    return parts.join(' || ');
+  };
+
+  exports.startsWith = function(s, p) {
+    return s.indexOf(p) === 0;
+  };
+
+  exports.endsWith = function(s, p) {
+    return s.indexOf(p, s.length - p.length) !== -1;
+  };
+
+  exports.isGraphiteName = function(name) {
+    return exports.startsWith(name, 'stats.');
+  };
+
+  exports.translateGraphiteName = function(name) {
+    var slug;
+    if (exports.startsWith(name, 'stats.timers.')) {
+      var arr = name.split('.');
+      slug = arr.slice(2, arr.length-1).join('.');
+      return 'timer.' + arr[arr.length-1] + '.' + slug;
+    }
+    if (exports.startsWith(name, 'stats.gauges.')) {
+      slug = name.slice('stats.gauges.'.length);
+      return 'gauge.' + slug;
+    }
+    if (exports.startsWith(name, 'stats.')) {
+      return 'counter.' + name.slice('stats.'.length);
     }
   };
+
+  return exports;
 };
