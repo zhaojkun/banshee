bde037b7444682aa79bbbb22eebf6476b9d9300f
hit9 <hit9@icloud.com>
2016-03-23 17:37:47 +0800

    Remove api getNotice

--- webapp/config.go
+++ webapp/config.go
@@ -24,11 +24,6 @@ func getInterval(w http.ResponseWriter, r *http.Request, _ httprouter.Params) {
 	ResponseJSONOK(w, &intervalResponse{cfg.Interval})
 }
 
-// getNoice returns config.webapp.notice.
-func getNotice(w http.ResponseWriter, r *http.Request, _ httprouter.Params) {
-	ResponseJSONOK(w, cfg.Webapp.Notice)
-}
-
 // getLanguage returns config.webapp.language.
 type languageResponse struct {
 	Language string `json:"language"`
