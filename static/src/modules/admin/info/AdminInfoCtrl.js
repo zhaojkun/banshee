87ca9e45af0c15b9bd8cd69f12d8643f79e9f499
hit9 <hit9@icloud.com>
2016-05-18 14:28:55 +0800

    Yse fixed .4 for metricCacheInitCost

--- static/src/modules/admin/info/AdminInfoCtrl.js
+++ static/src/modules/admin/info/AdminInfoCtrl.js
@@ -1,20 +1,20 @@
 /*@ngInject*/
-module.exports = function ($scope, Info) {
+module.exports = function($scope, Info) {
   $scope.info = {};
 
-  $scope.loadData = function () {
-    Info.get().$promise
-      .then(function (res) {
-        if (Object.keys(res).length === 0) {
-          $scope.info = null;
-        } else {
-          // Tofixed with cost
-          res.detectionCost = res.detectionCost.toFixed(4);
-          res.filterCost = res.filterCost.toFixed(4);
-          res.queryCost = res.queryCost.toFixed(4);
-          $scope.info = res;
-        }
-      });
+  $scope.loadData = function() {
+    Info.get().$promise.then(function(res) {
+      if (Object.keys(res).length === 0) {
+        $scope.info = null;
+      } else {
+        // Tofixed with cost
+        res.detectionCost = res.detectionCost.toFixed(4);
+        res.filterCost = res.filterCost.toFixed(4);
+        res.queryCost = res.queryCost.toFixed(4);
+        res.metricCacheInitCost = res.metricCacheInitCost.toFixed(4);
+        $scope.info = res;
+      }
+    });
   };
 
   $scope.loadData();
