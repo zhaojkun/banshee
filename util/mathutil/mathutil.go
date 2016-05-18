590a0f055f38b3559a1a0ab41c166f074e00c798
hit9 <hit9@icloud.com>
2016-05-18 12:02:04 +0800

    Returns 0 if the given vals is empty on util Average and StdDev

--- util/mathutil/mathutil.go
+++ util/mathutil/mathutil.go
@@ -6,7 +6,11 @@ package mathutil
 import "math"
 
 // Average returns the mean value of float64 values.
+// Returns zero if the vals length is 0.
 func Average(vals []float64) float64 {
+	if len(vals) == 0 {
+		return 0
+	}
 	var sum float64
 	for i := 0; i < len(vals); i++ {
 		sum += vals[i]
@@ -16,7 +20,11 @@ func Average(vals []float64) float64 {
 
 // StdDev returns the standard deviation of float64 values, with an input
 // average.
+// Returns zero if the vals length is 0.
 func StdDev(vals []float64, avg float64) float64 {
+	if len(vals) == 0 {
+		return 0
+	}
 	var sum float64
 	for i := 0; i < len(vals); i++ {
 		dis := vals[i] - avg
