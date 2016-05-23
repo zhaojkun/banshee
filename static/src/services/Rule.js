31e15365995d392aea5e7f3b792bf928c31685ed
hit9 <hit9@icloud.com>
2016-05-23 18:16:27 +0800

    Use PATCH instead of POST in editRule

--- static/src/services/Rule.js
+++ static/src/services/Rule.js
@@ -2,15 +2,9 @@
  * Created by Panda on 16/1/16.
  */
 /*@ngInject*/
-module.exports = function ($resource) {
+module.exports = function($resource) {
   return $resource('/api/rule/:id', {projectId: '@projectId', id: '@id'}, {
-    save: {
-      method: 'POST',
-      url: '/api/project/:projectId/rule'
-    },
-    update: {
-      method: 'POST',
-      url: '/api/rule/:id'
-    }
+    save: {method: 'POST', url: '/api/project/:projectId/rule'},
+    update: {method: 'PATCH', url: '/api/rule/:id'}
   });
 };
