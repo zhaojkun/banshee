f04d86008c62c7f713f0f1bb54c0b894001f688b
hit9 <hit9@icloud.com>
2016-05-25 15:47:16 +0800

    Minor fix comments

--- storage/doc.go
+++ storage/doc.go
@@ -6,14 +6,14 @@ Package storage implements banshee's persistence storage.
 
 Structure
 
-Storage handles 4 kinds of data: index, metric and admin, the directory
+Storage handles 4 kinds of data: index, metric, event and admin, the directory
 structure is:
 
 	storage/
 	    |--index/               --- Metric index          LevelDB
 	    |--metric/              --- Metric data           LevelDB
 	    |--admin                --- Rules/Users/Projects  SQLite3
-		|- event/               --- Event                 SQLite3
+	    |- event/               --- Event                 SQLite3
 
 The storage directory will be created if not exists.
 
