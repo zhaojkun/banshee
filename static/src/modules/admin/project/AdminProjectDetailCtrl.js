9146b82db5f2199b677ec5edf466b8d0feba1fb5
hit9 <hit9@icloud.com>
2016-05-26 15:31:23 +0800

    Lazy load project events

--- static/src/modules/admin/project/AdminProjectDetailCtrl.js
+++ static/src/modules/admin/project/AdminProjectDetailCtrl.js
@@ -36,11 +36,6 @@ module.exports =
     // get config
     Config.get().$promise.then(function(res) { $scope.config = res; });
 
-    // get events
-    Project.getEventsByProjectId({id: projectId})
-        .$promise.then(function(res) { $scope.events = res; });
-
-    watchEventLoadParams();
   };
 
   $scope.edit = function() {
@@ -247,12 +242,18 @@ module.exports =
         .$promise.then(function(res) { $scope.events = res; });
   };
 
-  function watchEventLoadParams() {
+  $scope.watchEventLoadParams = function() {
+    if ($scope.watchEventLoadParamsDone) {
+      return;
+    }
     $scope.$watchGroup(['eventPast', 'eventLevel'], function() {
       $scope.events = null;
       $scope.loadEvents();
-    })
-  }
+    });
+    $scope.watchEventLoadParamsDone = true;
+  };
+
+  $scope.watchEventLoadParamsDone = false;
 
   $scope.loadData();
 
