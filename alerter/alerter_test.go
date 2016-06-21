bc136ca073c773a991d4c4c4a79ead040369b945
WooParadog <haochuan.guo@ele.me>
2016-06-21 21:14:30 +0800

    Add unittest

--- alerter/alerter_test.go
+++ alerter/alerter_test.go
@@ -3,8 +3,11 @@
 package alerter
 
 import (
-	"github.com/eleme/banshee/util"
 	"testing"
+
+	"github.com/eleme/banshee/models"
+	"github.com/eleme/banshee/util"
+	"github.com/eleme/banshee/util/safemap"
 )
 
 func TestHourInRange(t *testing.T) {
@@ -14,4 +17,20 @@ func TestHourInRange(t *testing.T) {
 	util.Must(t, hourInRange(23, 19, 10))
 	util.Must(t, hourInRange(6, 19, 10))
 	util.Must(t, !hourInRange(13, 19, 10))
+}
+
+func TestAlertRecord(t *testing.T) {
+	a := &Alerter{alertRecords: safemap.New()}
+	metrics := &models.Metric{Name: "test", Stamp: 80, Value: 80}
+	util.Must(t, hourInRange(23, 19, 10))
+	util.Must(t, !a.checkAlertCount(metrics))
+	metrics.Stamp = 81
+	a.setAlertRecord(metrics)
+	metrics.Stamp = 82
+	a.setAlertRecord(metrics)
+	metrics.Stamp = 83
+	a.setAlertRecord(metrics)
+	metrics.Stamp = 84
+	a.setAlertRecord(metrics)
+	util.Must(t, a.checkAlertCount(metrics))
 }
