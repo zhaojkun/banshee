6294726cc1a9d20766f70157790873073b8bf37d
647 <liusiqi@ustil.cn>
2017-09-14 14:01:02 +0800

    Add Message note

--- models/cache.go
+++ models/cache.go
@@ -13,9 +13,10 @@ type cache struct {
 	shared bool
 }
 
+// Message is a add or delete event.
 type Message struct {
 	Type string       `json:"type"`
-	Rule *Rule `json:"rule"`
+	Rule *Rule        `json:"rule"`
 }
 
 // Share the instance between goroutines.
